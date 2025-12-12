12e2345fa6632323372ead9d7449f2b580736a50
MrBohRyk <lyutyk2004@gmail.com>
2025-12-12 16:14:59 +0100

    letters ascii

--- lection0/crack.py
+++ lection0/crack.py
@@ -1,6 +1,6 @@
-from string import digits # імпорт набору цифр '0123456789'
-for i in digits:          # перша позиція
-    for j in digits:      # друга позиція
-        for k in digits:   # третя позиція
-            for l in digits:  # четверта позиція
+from string import ascii_letters # імпорт набору цифр '0123456789'
+for i in ascii_letters:          # перша позиція
+    for j in ascii_letters:      # друга позиція
+        for k in ascii_letters:   # третя позиція
+            for l in ascii_letters:  # четверта позиція
                 print(i, j, k, l) # вивести комбінацію з пробілами між цифрами
